7da3a97c4c54fdd68f7ded6b5df026052e5f7983
Bartosz Ziętak <bartekzietak01@gmail.com>
2021-05-19 01:09:02 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,3 +7,9 @@ function printMessage(msg){
 function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
 }
+
+
+var computerMove, playerMove;
+computerMove = 'kamień';
+playerMove = 'papier';
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to ' + playerMove + ', to wygrywasz!');
