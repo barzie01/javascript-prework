7f3a1ebb5251a9eef01c4acd570ca40e4719007c
Bartosz Ziętak <bartekzietak01@gmail.com>
2021-05-19 21:25:25 +0200

    Update script

--- js/script.js
+++ js/script.js
@@ -9,7 +9,14 @@ function clearMessages(){
 }
 
 
-var computerMove, playerMove;
-computerMove = 'kamień';
-playerMove = 'papier';
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to ' + playerMove + ', to wygrywasz!');
+var computerMove, randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('wylosowana liczba to: ' + randomNumber);
+if (randomNumber == '1') {
+  computerMove = 'kamień';
+} else if (randomNumber == '2') {
+  computerMove = 'papier';
+} else {
+  computerMove = 'nożyce';
+}
+printMessage('Mój ruch: ' + computerMove);
