d5addeebdeada02f545305492b56a24308530ce0
Bartosz Ziętak <bartekzietak01@gmail.com>
2021-05-26 00:35:56 +0200

    Remove declatation var playerInput

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-var playerInput, playerMove, randomNumber, playerWin, computerWin, argplayerWin, argcomputerWin;
+var playerMove, randomNumber, playerWin, computerWin, argplayerWin, argcomputerWin;
 var playerWin = 0;
 var computerWin = 0;
 
